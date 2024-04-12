2e306e1b4675a1a0855a2e264f2da149c90fc04b
RinatSa <sadykov.r@seznam.cz>
2024-04-12 10:56:49 +0200

    Add new-user form(react-hook-form)

--- src/components/add-new-user.tsx
+++ src/components/add-new-user.tsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import {useForm} from "react-hook-form";
 
-function AddNewUser(props) {
+type Inputs = {
+    name: string
+    gender: string,
+    banned: string
+}
+
+function AddNewUser() {
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors},
+    } = useForm<Inputs>()
+
+    const onSubmit = (data: Inputs) => {
+        console.log({
+            id: 10,
+            name: data.name,
+            gender: data.gender,
+            banned: data.banned === 'false' ? false : !!data.banned
+        })
+        reset()
+    }
+
     return (
-        <div></div>
+        <div className="user-form__label">Add new user
+            <form onSubmit={handleSubmit(onSubmit)} className="form" id={"newUser"}>
+                <label> Name:</label>
+                <input {...register("name", {required: true, pattern: /^[A-Za-z. ]+$/i})}/>
+                <div className="error-message">
+                    {errors.name && <p>Please write the name (only letters)</p>}
+                </div>
+                <label> Gender:</label>
+                <select {...register("gender", {required: true})}>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                    <option value="other">other</option>
+                </select>
+                <label> Banned:</label>
+                <select {...register("banned", {required: true})}>
+                    <option value="false">false</option>
+                    <option value="true">true</option>
+                </select>
+                <input type="submit" value="SUBMIT" className="btn"/>
+            </form>
+        </div>
     );
 }
 
